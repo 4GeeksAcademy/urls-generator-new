a16c95e4da8726ed13bb8f2f6c6b8c7040fab1b8
Yohan Mayorga <102123553+yohanmayorga@users.noreply.github.com>
2023-08-08 15:13:03 +0000

    url-generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const pronouns = ["the", "my", "your"];
+  const adjectives = ["big", "small", "tall"];
+  const nouns = ["house", "car", "dog"];
+  const urls = [".com", ".net", ".es"];
+
+  const combinations = [];
+
+  for (const pronoun of pronouns) {
+    for (const adjective of adjectives) {
+      for (const noun of nouns) {
+        for (const url of urls) {
+          const combination = `${pronoun}${adjective}${noun}${url}`;
+          combinations.push(combination);
+        }
+      }
+    }
+  }
+
+  const ul = document.getElementById("combinations");
+
+  ul.innerHTML = "";
+
+  for (const combination of combinations) {
+    const li = document.createElement("li");
+    li.textContent = combination;
+    ul.appendChild(li);
+  }
 };
